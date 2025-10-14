6c7533e92788db06e56141608b7fca479bcd474b
Nico <a24nicolassc@iesantonlosada.gal>
2025-10-14 18:32:58 +0200

    Creamos una arrayList con los Jobs e invocamos al Yaml.Hacemos un try-catch con InputStream para para coger todos los recursos de JobLoader.Después hacemos un map para imprimir los jobs,y pot último un List de ese map,ya que vamos a cargar mas de un Job.

--- src/main/java/jobs/JobLoader.java
+++ src/main/java/jobs/JobLoader.java
@@ -4,9 +4,30 @@
  */
 package jobs;
 
+import AppConfig.Job;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import org.yaml.snakeyaml.Yaml;
+
 
 public class JobLoader {
     
+    public static List<Job> loadJobs(String path) throws IOException{
     
+        List<Job> jobsList = new ArrayList<>();
+        Yaml yaml = new Yaml();
+        
+        try(InputStream input = JobLoader.class.getResourceAsStream(path)){
+            if(input ==null){
+                    throw new RuntimeException("No se encontro el YAML" + path);
+            
+            }
+        
+        }
+    
+    
+    }
     
 }
